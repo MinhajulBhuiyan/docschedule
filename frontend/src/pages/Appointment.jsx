7282ef7ac206c0fca3e0f80ca9adf60842c23b63
RIFAF RAHMAN <127125526+codebyrifaf@users.noreply.github.com>
2025-06-12 17:19:21 +0600

    #9: Adding Doctors Components

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -7,6 +7,7 @@ const Appointment = () => {
 
   const {docId} = useParams()
   const {doctors,currencySymbol} = useContext(AppContext)
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
   const [docInfo, setDocInfo] = useState(null)
   const [docSlots,setDocSlots]= useState([])
@@ -19,13 +20,48 @@ const Appointment = () => {
     setDocInfo(docInfo)
   }
 
-  const getAvailableSlots = async() = {
+  const getAvailableSlots = async() => {
     setDocSlots([])
     //getting current state 
     let today = new Date()
 
     for(let i=0; i<7;i++){
+      //getting data with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
 
+      // setting end time of the data with index
+      let endTime = new Date()
+      endTime.setData(today.getDate()+i)
+      endTime.setHours(21,0,0,0)
+
+      //setting hours
+      if(today.getDate()=== cuurentDate.getDate()){
+        currentDate.setHours(cuurentDate.getHours()>10?currentDate.getHours()+1:10)
+        currentDate.setMinutes(currentDate.getMinutes()>30?30:0)
+
+      }else{
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+      let timeSlots =[]
+
+      while(currentDate<endTime){
+        let formattedTime = currentDate.toLocaleDateString([],{hour: '2-digit', minute: '2-digit'})
+
+        //add slot to array
+        timeSlots.push({
+          datetime:new Date(currentDate),
+          time: formattedTime
+        })
+
+        //Increment current time by 30 minutes 
+        currentDate.setMinutes(currentDate.getMinutes()+30)
+      }
+
+      setDocSlots(prev=>{[...prev,timeSlots]})
+
+      
     }
 
   }
@@ -37,6 +73,10 @@ const Appointment = () => {
   useEffect(()=>{
     getAvailableSlots()
   },[docInfo])
+
+  useEffect(()=>{
+    console.log(docSlots);
+  },[docSlots])
 
   return docInfo && (
     <div>
@@ -71,8 +111,22 @@ const Appointment = () => {
                 Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
               </p>
           </div>
-
         </div>
+
+      {/*--------------Booking Slots--------------- */}
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking slots</p>
+        <div>
+          {
+            docSlots.length && docSlots.map((item,index)=>(
+              <div key={index}>
+                <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
