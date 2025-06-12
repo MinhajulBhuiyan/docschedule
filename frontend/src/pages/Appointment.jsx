71c22d85bc2461a10d1a3855028835d92354dc72
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-06-12 23:16:50 +0600

    #10: Create appointment scheduling form layout and basic logic

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -128,11 +128,17 @@ const Appointment = () => {
         </div>
         <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
           {docSlots.length && docSlots[slotIndex].map((item,index)=>(
-            <p className={'text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer'} key={index}>
-              {item.time.toLowerCase()}
-            </p>
-          ))}
+        <p
+          key={index}
+          onClick={() => setSlotTime(item.time)}
+          className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`}
+        >
+        {item.time.toLowerCase()}
+        </p>
+
+        ))}
         </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
       </div>
     </div>
   )
