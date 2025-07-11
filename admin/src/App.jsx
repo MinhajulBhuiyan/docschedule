0402a56c0f81298749826d07bc56047b0473d197
bhuiyan.minhajul@gmail.com <bhuiyan.minhajul@gmail.com>
2025-07-12 01:05:18 +0600

    #18: Add admin sidebar and navbar components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,41 @@
+import React, { useContext } from 'react';
+import Login from './pages/login';
+import { Slide, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Slidebar from './components/Slidebar';
 import { Routes, Route } from 'react-router-dom';
-import React from 'react';
-import Login from './pages/login.jsx';
-import './index.css';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      {/* other routes */}
-    </Routes>
-  );
+const App = () => {
+
+  const { aToken } = useContext(AdminContext);
+
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer/>
+      <Navbar />
+      <div className='flex items-start'>
+        <Slidebar />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList/>} />
+        </Routes>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer/>        
+    </>
+  )
 }
-export default App;
+
+export default App
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react'
 import { assets } from '../assets/assets'
 import axios from 'axios'
 import { AdminContext } from '../context/AdminContext'
+import { toast } from 'react-toastify'
 const Login = () => {
     const [state, setState] = useState('Admin')
     const [email, setEmail] = useState('')
@@ -15,9 +16,10 @@ const Login = () => {
             if(state==='Admin'){
             const {data} = await axios.post(backendUrl+'/api/admin/login', {email, password})
             if(data.success){
-                console.log(data.token)
+                localStorage.setItem('aToken', data.token)
+                setAToken(data.token)
             } else{
-
+                toast.error(data.message)
             }
         }
         } catch (error) {
