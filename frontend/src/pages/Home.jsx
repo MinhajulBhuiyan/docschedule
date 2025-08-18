48bdded463467734ad85bc419e2592644d0926b9
Medha <tasniaanwer@gmail.com>
2025-08-19 00:52:53 +0600

    navbar edit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
@@ -7,7 +6,6 @@ import Banner from '../components/Banner'
 const Home = () => {
   return (
     <div>
-      <Header />
       <SpecialityMenu />
       <TopDoctors />
       <Banner />
